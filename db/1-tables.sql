7a3f97fe5dd9faaaa24f9e9769a4e0f806750e84
Pandraghon <pandraghon@outlook.com>
2023-10-16 11:33:56 +0000

    add: 2022 database schema

---
+++ db/1-tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE answers (
+    id serial PRIMARY KEY,
+    question_id integer NOT NULL,
+    content character varying NOT NULL
+);
+
+COMMENT ON COLUMN answers.question_id IS 'ID of the question on GW2Trivia';
+COMMENT ON COLUMN answers.content IS 'Expected answer(s)';
+
+CREATE TABLE participations (
+    id serial PRIMARY KEY,
+    day integer NOT NULL REFERENCES questions(day),
+    account character varying NOT NULL,
+    date timestamp without time zone DEFAULT LOCALTIMESTAMP,
+    valid boolean DEFAULT false,
+    ip character varying,
+    content text
+);
+
+COMMENT ON COLUMN participations.account IS 'GW2 Account name';
+COMMENT ON COLUMN participations.date IS 'Datetime of the submit';
+COMMENT ON COLUMN participations.valid IS 'Whether the answer is valid';
+COMMENT ON COLUMN participations.ip IS 'IP of the request';
+COMMENT ON COLUMN participations.content IS 'Answer content';
+
+CREATE TABLE questions (
+    day integer PRIMARY KEY,
+    question_id integer,
+    title text,
+    displayed_response character varying
+);
+
+COMMENT ON COLUMN questions.question_id IS 'ID of the question on GW2Trivia';
+COMMENT ON COLUMN questions.title IS 'Question title';
+COMMENT ON COLUMN questions.displayed_response IS 'Human-readable response';
